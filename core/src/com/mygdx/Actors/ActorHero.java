3536a7b21f953ae71bea67281bb26b1ba349d82f
Zitinger <72313082+Zitinger@users.noreply.github.com>
2021-05-27 20:07:05 +0200

    управление клавиатурой

--- core/src/com/mygdx/Actors/ActorHero.java
+++ core/src/com/mygdx/Actors/ActorHero.java
@@ -1,14 +1,22 @@
 package com.mygdx.Actors;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 
 
 public class ActorHero extends Actor {
 
-    private TextureRegion textureHero;
+    private Texture textureHero;
     private boolean alive;
+    private Sprite spriteHero;
+    //MoveByAction move = new MoveByAction();
+    final float STEP = 10f;
 
     public boolean isAlive() {
         return alive;
@@ -18,15 +26,42 @@ public class ActorHero extends Actor {
         this.alive = alive;
     }
 
-    public ActorHero(TextureRegion textureHero){
+    public ActorHero(Texture textureHero){
         this.textureHero=textureHero;
         this.alive=true;
-        setSize(textureHero.getRegionWidth(), textureHero.getRegionHeight());
+        spriteHero = new Sprite(textureHero);
+        setSize(spriteHero.getRegionWidth(), spriteHero.getRegionHeight());
     }
 
     public void act (float delta)
     {
         super.act(delta);
+        if(Gdx.input.isKeyPressed(Input.Keys.UP))
+        {
+            textureHero = new Texture("CharacterTilesSet-Left.png");
+            spriteHero = new Sprite(textureHero);
+            spriteHero.setPosition(getX() - STEP - 5, getY());
+            this.setPosition(getX() - STEP - 5, getY());
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+        {
+            textureHero = new Texture("CharacterTilesSet-Right.png");
+            spriteHero = new Sprite(textureHero);
+            spriteHero.setPosition(getX() + STEP + 5, getY());
+            this.setPosition(getX() + STEP + 5, getY());
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+        {
+            spriteHero = new Sprite(textureHero);
+            spriteHero.setPosition(getX(), getY() - STEP);
+            this.setPosition(getX(), getY() - STEP);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+        {
+            spriteHero = new Sprite(textureHero);
+            spriteHero.setPosition(getX(), getY() + STEP);
+            this.setPosition(getX(), getY() + STEP);
+        }
     }
 
     public void draw (Batch batch, float parentAlpha){
--- core/src/com/mygdx/Screens/MainGameScreen.java
+++ core/src/com/mygdx/Screens/MainGameScreen.java
@@ -1,35 +1,52 @@
 package com.mygdx.Screens;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.mygdx.Actors.ActorHero;
 
 
-public class MainGameScreen extends BaseScreen {
-    public MainGameScreen(MainGame game) {
+public class MainGameScreen extends BaseScreen implements ApplicationListener
+{
+    public MainGameScreen(MainGame game)
+    {
         super(game);
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        batch = new SpriteBatch();
+
         textureHero = new Texture("CharacterTilesSet-Right.png");
-        regionHero = new TextureRegion(textureHero, textureHero.getWidth(), textureHero.getHeight());
+        spriteHero = new Sprite(textureHero);
+//        spriteHero.setPosition(w/2 - spriteHero.getWidth()/2, h/2 - spriteHero.getHeight()/2);
+
     }
 
+    private SpriteBatch batch;
     private Stage stage;
     private ActorHero actorHero;
     private Texture textureHero;
-    private TextureRegion regionHero;
+    private Sprite spriteHero;
+    private MoveToAction move;
 
 
 
     @Override
     public void show() {
         stage = new Stage();
-        actorHero = new ActorHero(regionHero);
+        actorHero = new ActorHero(textureHero);
+        actorHero.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+//        move = new MoveToAction();
+//        move.setActor(actorHero);
         stage.addActor(actorHero);
-        actorHero.setPosition(stage.getWidth() / 3, stage.getHeight() / 2);
-        actorHero.rotateBy(180);
-        actorHero.act(100);
+//        actorHero.rotateBy(180);
+//        actorHero.act(100);
     }
 
     @Override
@@ -42,14 +59,24 @@ public class MainGameScreen extends BaseScreen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         stage.act();
         stage.draw();
     }
 
 
     @Override
+    public void create()
+    {}
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
     public void dispose()
     {
-        textureHero.dispose();
+        batch.dispose();
     }
 }
